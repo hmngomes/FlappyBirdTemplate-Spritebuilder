99af645042584621fff6ee9654e7316da6593546
Francisco Reis Gomes <francisco@hidra.local>
2015-05-06 22:11:08 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,17 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    
 }
 
 // put new methods here
+
 
 @end
